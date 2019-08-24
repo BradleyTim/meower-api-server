a166fadb264ce676a555b4ab7ee68bfba15c52b1
BradleyTim <timbradley94@gmail.com>
2019-08-24 12:48:25 +0300

    add cors middleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 app.get('/', (request, response) => {
   response.json({
