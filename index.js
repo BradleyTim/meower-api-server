0c1976a325ebfb0bd26c9871b4bbb8f141f6f539
BradleyTim <timbradley94@gmail.com>
2019-08-24 12:34:44 +0300

    add get and post routes

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.json({
+    message: 'meower 😹'
+  });
+});
+
+app.post('/meowers', (request, response) => {
+  console.log(request.body);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
 });
